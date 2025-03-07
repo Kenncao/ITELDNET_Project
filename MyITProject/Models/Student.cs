853ebaf5606a0632442877bdf467635548479b17
90994-25257866 <90994-25257866@CLAB4-28>
2025-03-07 11:43:00 +0800

    Add project files.

---
+++ MyITProject/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace MyITProject.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Course { get; set; }
+
+    }
+}
